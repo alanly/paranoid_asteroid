8d132d03ce8aab3ae0ccd7294f1f467300adf2d3
Alex Coco <hello@alexcoco.com>
2013-03-24 18:03:55 -0400

    Added bullet fired list and logic to notify listeners when bullet is fired

--- src/game/entities/Ship.java
+++ src/game/entities/Ship.java
@@ -1,40 +1,60 @@
 package game.entities;
 
+import game.BulletFiredEvent;
+import game.BulletFiredListener;
 import game.GameField;
 import game.InputHandler;
 import game.Point;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Ship extends Entity {
+	private final static double BULLET_FIRE_DELAY = 5e8;
 	private final static double MAX_LINEAR_SPEED = 3.4e-7;
 	private final static double MIN_ANGULAR_SPEED = 5e-9;
 	private final static double ACCELERATION = 1.0e-15;
 
 	private Point[] vertices;
+	private List<BulletFiredListener> bulletFiredListeners;
 
 	private double linearSpeed = 0;
 	private double angle = Math.PI / 2;
+	private long lastFired = 0;
 
 	/**
-	 * Constructs a new ship
+	 * Constructs a new ship.
 	 * @param center the center of the ship
 	 */
 	public Ship(Point center) {
 		this.setCenter(center);
+		bulletFiredListeners = new LinkedList<BulletFiredListener>();
 		initializeVertices();
 	}
 
 	/**
-	 * Updates the state of the ship
+	 * Updates the state of the ship and fires bullets.
 	 * @param delta the time since the last update
 	 */
 	public void update(long delta) {
 		updateSpeed(delta);
 		updateAngle(delta);
 		updateVertices(delta);
+		
+		long now = System.nanoTime();
+		
+		if (InputHandler.getInstance().getSpaceKey().isPressed() && (now - lastFired) > BULLET_FIRE_DELAY) {
+			lastFired = now;
+			
+			for (BulletFiredListener listener : bulletFiredListeners) {
+				// Origin is the tip of the ship, the first vertex
+				listener.bulletFired(new BulletFiredEvent(this, now, vertices[0], angle));
+			}
+		}
 	}
 
 	/**
-	 * Updates the speed of the ship
+	 * Updates the speed of the ship.
 	 * @param delta the time since the last update
 	 */
 	public void updateSpeed(long delta) {
@@ -48,9 +68,17 @@ public class Ship extends Entity {
 			linearSpeed = Math.max(0, linearSpeed);
 		}
 	}
+	
+	/**
+	 * Adds a bullet fired listener to the list of listeners to notify when a bullet is fired.
+	 * @param l bullet fired listener to notify when the bullet is fired
+	 */
+	public void addBulletFiredListener(BulletFiredListener l) {
+		this.bulletFiredListeners.add(l);
+	}
 
 	/**
-	 * Updates the angle of the ship
+	 * Updates the angle of the ship.
 	 * @param delta the time since the last update
 	 */
 	private void updateAngle(long delta) {
@@ -76,7 +104,7 @@ public class Ship extends Entity {
 	}
 
 	/**
-	 * Updates the position of each vertex of the ship
+	 * Updates the position of each vertex of the ship.
 	 * @param delta the time since the last update
 	 */
 	private void updateVertices(long delta) {
@@ -96,7 +124,7 @@ public class Ship extends Entity {
 	}
 	
 	/**
-	 * Returns the ship's vertices
+	 * Returns the ship's vertices.
 	 * @return the ship's vertices
 	 */
 	public Point[] getVertices() {
@@ -104,7 +132,7 @@ public class Ship extends Entity {
 	}
 
 	/**
-	 * Initializes the vertices of the ship
+	 * Initializes the vertices of the ship.
 	 */
 	private void initializeVertices() {
 		this.vertices = new Point[] {
