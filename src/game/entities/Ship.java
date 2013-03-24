45d4d92191a47dcdbcbdf442623c0b684ca60e98
Alex Coco <hello@alexcoco.com>
2013-03-24 15:58:54 -0400

    Ship moves, changed constructor

--- src/game/entities/Ship.java
+++ src/game/entities/Ship.java
@@ -1,22 +1,88 @@
 package game.entities;
 
+import game.GameField;
+import game.InputHandler;
 import game.Point;
 
 public class Ship extends Entity {
-	public static final double MAX_SPEED = 300.0;
-	public static final double ACCELERATION = 100.0;
-	
-	public Ship(double x, double y) {
-		this.setCenter(new Point(x, y));
-	}
-	
+	private final static double MAX_LINEAR_SPEED = 3.4e-7;
+	private final static double MIN_ANGULAR_SPEED = 5e-9;
+	private final static double ACCELERATION = 1.0e-15;
+
+	private Point[] vertices;
+
+	private double linearSpeed = 0;
+	private double angle = Math.PI / 2;
+
 	public Ship(Point center) {
-		this.setCenter(new Point(center.x, center.y));
-		System.out.println(this.getCenter());
+		this.setCenter(center);
+		initializeVertices();
+	}
+
+	public void update(long delta) {
+		updateSpeed(delta);
+		updateAngle(delta);
+		updateVertices(delta);
+	}
+
+	public void updateSpeed(long delta) {
+		if (InputHandler.getInstance().getUpKey().isPressed()) {
+			// Accelerate
+			linearSpeed += ACCELERATION * delta;
+			linearSpeed = Math.min(MAX_LINEAR_SPEED, linearSpeed);
+		} else {
+			// Decelerate
+			linearSpeed -= ACCELERATION * delta;
+			linearSpeed = Math.max(0, linearSpeed);
+		}
+	}
+
+	private void updateAngle(long delta) {
+		double deltaAngle = 0;
+		double angularSpeed = MIN_ANGULAR_SPEED
+				* (1 - linearSpeed / (2 * MAX_LINEAR_SPEED));
+
+		if (InputHandler.getInstance().getLeftKey().isPressed()) {
+			// Turn CCW
+			deltaAngle = -angularSpeed * delta;
+		} else if (InputHandler.getInstance().getRightKey().isPressed()) {
+			// Turn CW
+			deltaAngle = angularSpeed * delta;
+		}
+
+		// Rotate each point by the change in the angle
+		for (Point vertice : vertices) {
+			vertice.rotate(getCenter(), deltaAngle);
+		}
+
+		// Update the angle
+		angle = (angle - deltaAngle) % FULL_CIRCLE_RAD;
+	}
+
+	private void updateVertices(long delta) {
+		double distance = linearSpeed * delta;
+		double dx = distance * Math.cos(angle);
+		double dy = distance * Math.sin(angle);
+
+		// Move center
+		getCenter().move(dx, -dy);
+
+		// Move vertices
+		for (Point vertex : vertices) {
+			vertex.move(dx, -dy);
+		}
+		
+		getCenter().wrapAround(GameField.WIDTH, GameField.HEIGHT, vertices);
 	}
 	
-	@Override
-	public void update(long delta) {
-		
+	public Point[] getVertices() {
+		return vertices;
+	}
+
+	private void initializeVertices() {
+		this.vertices = new Point[] {
+				new Point(getX(), getY() - 10),
+				new Point(getX() - 5, getY() + 5),
+				new Point(getX() + 5, getY() + 5) };
 	}
 }
