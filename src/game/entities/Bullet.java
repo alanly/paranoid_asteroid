cf770b7c78a81fcb8f5342ec61c3caca0744ab25
Alexander Coco <hello@alexcoco.com>
2013-03-24 18:58:07 -0400

    Removed drag, speeds make more sense now

--- src/game/entities/Bullet.java
+++ src/game/entities/Bullet.java
@@ -7,9 +7,8 @@ import java.awt.Color;
 
 public class Bullet extends Entity {
 	private static final double MAX_TIME_TO_LIVE = 2.0e9;
-	private static final double MIN_LINEAR_SPEED = 1.0e-7;
+	private static final double LINEAR_SPEED = 4.0e-7;
 	private static final double MAX_LINEAR_SPEED = 4.0e-7;
-	private static final double DRAG_COEFFICIENT = 2.0e-3;
 
 	private boolean expired;
 	private Entity source;
@@ -34,19 +33,12 @@ public class Bullet extends Entity {
 		
 		timeToLive -= delta;
 
-		updateSpeed(delta);
 		updateColor();
 		updateCenter(delta);
 
 		if (timeToLive < 0) {
 			this.expired = true;
 		}
-	}
-
-	private void updateSpeed(long delta) {
-		// Imitates the effect of drag without all the fancy real math, it's good enough
-		linearSpeed = Math.max(MIN_LINEAR_SPEED, linearSpeed - linearSpeed
-				* linearSpeed * delta * DRAG_COEFFICIENT);
 	}
 
 	private void updateColor() {
