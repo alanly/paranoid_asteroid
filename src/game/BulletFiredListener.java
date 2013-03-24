fb06af05c5cfc623fc72e7ea24837dda337bc684
Alex Coco <hello@alexcoco.com>
2013-03-24 17:47:42 -0400

    Added bullet fired listener interface

---
+++ src/game/BulletFiredListener.java
@@ -0,0 +1,9 @@
+package game;
+
+/**
+ * A bullet fired listener will have its bullet fired event called when a BulletFiredEvent occurs.
+ *
+ */
+public interface BulletFiredListener {
+	public void bulletFired(BulletFiredEvent e);
+}
