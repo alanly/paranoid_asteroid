011854a4f2abd26dff700dc2c85ab6546704b0f5
Alexander Coco <hello@alexcoco.com>
2013-03-23 21:13:36 -0400

    Added docs

--- src/game/Entity.java
+++ src/game/Entity.java
@@ -8,28 +8,56 @@ public abstract class Entity {
 	
 	private Point center;
 	
+	/**
+	 * Updates the entity's state based on how much time has passed.
+	 * @param delta time since last update
+	 */
 	public abstract void update(long delta);
 	
+	/**
+	 * Gets the x-coordinate of the entity's center Point.
+	 * @return the x-coordinate of the entity's center Point
+	 */
 	public double getX() {
 		return center.x;
 	}
 	
+	/**
+	 * Gets the y-coordinate of the entity's center Point.
+	 * @return the y-coordinate of the entity's center Point
+	 */
 	public double getY() {
 		return center.y;
 	}
 	
+	/**
+	 * Gets the the entity's center Point.
+	 * @return the entity's center Point
+	 */
 	public Point getCenter() {
 		return center;
 	}
 	
+	/**
+	 * Sets the entity's center Point x-coordinate.
+	 * @param x the entity's center Point x-coordinate
+	 */
 	public void setX(double x) {
 		center.x = x;
 	}
 	
+	/**
+	 * Sets the entity's center Point y-coordinate.
+	 * @param y the entity's center Point y-coordinate
+	 */
 	public void setY(double y) {
 		center.y = y;
 	}
 	
+	/**
+	 * Sets the entity's center Point.
+	 * @param center the entity's center Point
+	 */
 	public void setCenter(Point center) {
 		this.center = center;
 	}
