d012bc0827be918e9c9ebbc73cde4365efb53313
Alex Coco <hello@alexcoco.com>
2013-03-24 15:59:04 -0400

    Ship renders

--- src/game/Renderer.java
+++ src/game/Renderer.java
@@ -3,16 +3,36 @@ package game;
 import game.entities.Entity;
 import game.entities.Ship;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 
 public class Renderer {
+	private static Color ENTITY_COLOR = new Color(0xF0F0F0);
 	public static void render(Entity e, Graphics2D g) {
 		if (e instanceof Ship) {
 			renderShip((Ship) e, g);
 		}
 	}
 	
-	private static void renderShip(Ship e, Graphics2D g) {
+	private static void renderShip(Ship e, Graphics g) {
+		Point[] vertices = e.getVertices();
+		Color oldColor = g.getColor();
 		
+		int[] x = new int[vertices.length];
+		int[] y = new int[vertices.length];
+		
+		for (int i = 0; i < vertices.length; i++) {
+			x[i] = (int) vertices[i].getX();
+			y[i] = (int) vertices[i].getY();
+		}
+		
+		Polygon p = new Polygon(x, y, x.length);
+		
+		g.setColor(ENTITY_COLOR);
+		g.drawPolygon(p);
+		
+		g.setColor(oldColor);
 	}
 }
