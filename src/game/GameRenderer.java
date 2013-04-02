b1bad6dbabe5c9db4449e2548bf4d3ca2a329dc0
Alex Coco <hello@alexcoco.com>
2013-04-02 12:10:45 -0400

    Updated game loop, split up rendering

--- src/game/GameRenderer.java
+++ src/game/GameRenderer.java
@@ -1,11 +1,6 @@
 package game;
 
-import game.entities.Bullet;
-import game.entities.Entity;
-import game.entities.Ship;
-
-import java.util.List;
 
 public interface GameRenderer {
-	public void renderGame(Ship ship, List<Bullet> bullets, List<Entity> entities, long pointsFluid, int level);
+	public void renderGame(Game game);
 }
