98cee7ae76352dfb652f71a7047b93c856d3b975
Alex Coco <hello@alexcoco.com>
2013-04-07 02:32:55 -0400

    Moved sound init to main

--- src/game/Launcher.java
+++ src/game/Launcher.java
@@ -5,15 +5,12 @@ import game.ui.GameFrame;
 import javax.swing.SwingUtilities;
 
 public class Launcher implements Runnable {
-	static {
-		SoundEffect.init();
-	}
-	
 	public void run() {
 		new GameFrame();
 	}
 	
 	public static void main(String[] args) {
+		SoundEffect.init();
 		playBackgroundMusic();
 		SwingUtilities.invokeLater(new Launcher());
 	}
