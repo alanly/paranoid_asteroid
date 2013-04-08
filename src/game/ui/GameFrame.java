15101c72537b0649915087c5dbd2343dcd20f76c
Alex Coco <hello@alexcoco.com>
2013-04-08 11:44:43 -0400

    Removed unused import

--- src/game/ui/GameFrame.java
+++ src/game/ui/GameFrame.java
@@ -9,7 +9,6 @@ import java.awt.CardLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.WindowConstants;
 
 public class GameFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
