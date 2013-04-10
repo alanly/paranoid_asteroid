f3c00f8dbce7d75e1397bd6c3185078955324d1a
Alex Coco <hello@alexcoco.com>
2013-04-10 17:40:22 -0400

    Added flashing coin label

--- src/game/ui/menu/MainPanel.java
+++ src/game/ui/menu/MainPanel.java
@@ -9,13 +9,16 @@ public class MainPanel extends BasePanel {
 	
 	private TitlePanel titlePanel;
 	private ButtonPanel buttonPanel;
+	private InsertCoinLabel coinLabel;
 	
 	public MainPanel(GameFrame gameFrame) {
 		this.setLayout(new BorderLayout());
 		
 		titlePanel = new TitlePanel();
 		buttonPanel = new ButtonPanel(gameFrame);
+		coinLabel = new InsertCoinLabel();
 		
+		this.add(coinLabel, BorderLayout.NORTH);
 		this.add(titlePanel, BorderLayout.CENTER);
 		this.add(buttonPanel, BorderLayout.SOUTH);
 		
