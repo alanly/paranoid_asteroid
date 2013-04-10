5a2d6b1871fe23a8f729c4cb6df6294891f688b3
Alex Coco <hello@alexcoco.com>
2013-04-10 15:15:59 -0400

    Removed old powerup classes

--- src/game/entities/BoostPowerup.java
+++
@@ -1,9 +0,0 @@
-package game.entities;
-
-import game.Point;
-
-public class BoostPowerup extends Powerup {
-	public BoostPowerup(Point center) {
-		super(center);
-	}
-}
--- src/game/entities/TripleShotPowerup.java
+++
@@ -1,9 +0,0 @@
-package game.entities;
-
-import game.Point;
-
-public class TripleShotPowerup extends Powerup {
-	public TripleShotPowerup(Point center) {
-		super(center);
-	}
-}
