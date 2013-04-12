302d2fda1689b93b940fa9708286d3863b53f1f3
Alex Coco <hello@alexcoco.com>
2013-04-11 20:25:35 -0400

    Adjusted asteroid shape

--- src/game/entities/SmallAsteroid.java
+++ src/game/entities/SmallAsteroid.java
@@ -14,7 +14,7 @@ public class SmallAsteroid extends Asteroid {
 		
 		double x = this.getX(), y = this.getY();
 		this.vertices[0] = new Point(x - 4, y - 12);
-		this.vertices[1] = new Point(x + 7, y - 6);
+		this.vertices[1] = new Point(x + 9, y - 8);
 		this.vertices[2] = new Point(x + 15, y - 2);
 		this.vertices[3] = new Point(x + 10, y + 10);
 		this.vertices[4] = new Point(x - 2, y + 11);
