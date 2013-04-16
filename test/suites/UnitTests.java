4effdf567c7eca534e24bc53ced76e04be110923
Alex Coco <hello@alexcoco.com>
2013-04-16 09:14:52 -0400

    Updated test suite

--- test/suites/UnitTests.java
+++ test/suites/UnitTests.java
@@ -7,17 +7,20 @@ import org.junit.runners.Suite.SuiteClasses;
 import unit.AlienTest;
 import unit.AsteroidTest;
 import unit.BasicGameStateTest;
+import unit.BulletFiredEventTest;
 import unit.BulletTest;
 import unit.EntityTest;
 import unit.ParticleTest;
 import unit.PointTest;
 import unit.PowerupTest;
+import unit.RendererTest;
 import unit.ShipTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AlienTest.class, AsteroidTest.class, BasicGameStateTest.class,
-		BulletTest.class, EntityTest.class, ParticleTest.class,
-		PointTest.class, PowerupTest.class, ShipTest.class })
+		BulletFiredEventTest.class, BulletTest.class, EntityTest.class,
+		ParticleTest.class, PointTest.class, PowerupTest.class,
+		RendererTest.class, ShipTest.class })
 public class UnitTests {
 
 }
